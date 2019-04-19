2b444d1d3489ded92866cf06266da040cc1cbcbf
Sanket Dahegaonkar <dahegaonkarsanket@gmail.com>
2019-04-20 01:12:13 +0530

    Updates

---
+++ precache-manifest.ada9f7bc4eccd0ee5d7441f6e5723604.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "78cbc84324f2150fb57d",
+    "url": "/static/css/main.b1384af6.chunk.css"
+  },
+  {
+    "revision": "78cbc84324f2150fb57d",
+    "url": "/static/js/main.b61a8d06.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "625f3d7a3948f1be8e22",
+    "url": "/static/css/2.8c105a02.chunk.css"
+  },
+  {
+    "revision": "625f3d7a3948f1be8e22",
+    "url": "/static/js/2.2f3f7ed0.chunk.js"
+  },
+  {
+    "revision": "ea54fcab0a6a76473e57c28d1d0ad9e9",
+    "url": "/index.html"
+  }
+];
